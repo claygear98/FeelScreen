e66cfcabd04487afc46b17174b1e8aebc002b751
hnk1203 <hnk1203@naver.com>
2024-01-17 02:19:08 +0900

    form 내부 수정

--- feelscreen/src/components/Features/Sign/Sign-Up/SignUp.jsx
+++ feelscreen/src/components/Features/Sign/Sign-Up/SignUp.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { useState } from "react";
 import { useForm } from 'react-hook-form';
 import styled from "styled-components";
 
@@ -30,45 +29,6 @@ const Wrapper = styled.div`
 		}
 	`
 function SignUp() {
-	// const [username, setUsername] = useState('');
-	// const [phone, setPhone] = useState('');
-	// const [password, setPassword] = useState('');
-	// const [passwordcheck, setPasswordCheck] = useState('');
-	// const [code, setCode] = useState('');
-
-	// // 입력 값 변경 핸들러
-	// const handleUsername = (e) => {
-	// 	  setUsername(e.target.value);
-	// 	};
-
-	// const handlePhone = (e) => {
-	//   setPhone(e.target.value);
-	// };
-
-	// const handlePassword = (e) => {
-	//   setPassword(e.target.value);
-	// };
-
-	// const handlePasswordCheck = (e) => {
-	//   setPasswordCheck(e.target.value);
-	// };
-
-	// const codeCheck = (e) => {
-	//   setCode(e.target.value);
-	// };
-	// // 회원가입 버튼 클릭 핸들러
-	// const handleSignup = () => {
-	//   // 여기에서 실제 회원가입 로직을 처리할 수 있습니다.
-	//   // 예를 들면, 서버에 회원가입 요청을 보내는 등의 작업이 들어갈 수 있습니다.
-	//   console.log('회원가입 정보:', { username, phone, password, code });
-	// };
-
-	// const FeelForm = styled.form`
-
-	// `
-	// const InputInfo = styled.div`
-
-	// `
 	const {
 		register,
 		formState: { errors },
@@ -93,9 +53,9 @@ function SignUp() {
 							<label>아이디(닉네임):</label>
 							<input
 								type="text"
-								{...register('username', {
-									required: '아이디를 입력해주세요.',
-								})}
+								{...register('username')}
+								placeholder="아이디는 2~10자 여야 합니다."
+								required
 							/>
 							{errors.username && <span>{errors.username.message}</span>}
 						</div>
@@ -103,9 +63,9 @@ function SignUp() {
 							<label>전화번호:</label>
 							<input
 								type="text"
-								{...register('phone', {
-									required: '전화번호를 입력해주세요.',
-								})}
+								{...register('phone')}
+								placeholder="전화번호를 입력해 주세요."
+								required
 							/>
 							{errors.phone && <span>{errors.phone.message}</span>}
 						</div>
@@ -113,9 +73,9 @@ function SignUp() {
 							<label>인증번호 입력:</label>
 							<input
 								type="text"
-								{...register('code', {
-									required: '인증번호를 입력해주세요.',
-								})}
+								{...register('code')}
+								placeholder="인증번호를 입력해 주세요."
+								required
 							/>
 							{errors.code && <span>{errors.code.message}</span>}
 						</div>
@@ -123,9 +83,10 @@ function SignUp() {
 							<label>비밀번호:</label>
 							<input
 								type="password"
-								{...register('password', {
-									required: '비밀번호를 입력해주세요.',
-								})}
+								{...register('password')}
+								placeholder="비밀번호를 입력해 주세요."
+								required
+								autoComplete="new-password"
 							/>
 							{errors.password && <span>{errors.password.message}</span>}
 						</div>
@@ -133,8 +94,7 @@ function SignUp() {
 							<label>비밀번호 확인:</label>
 							<input
 								type="password"
-								{...register('passwordCheck', {
-									required: '비밀번호를 한번더 입력해주세요.',
+								{...register('passwordCheck',{
 									validate: {
 										check: (val) => {
 											if (getValues('password') !== val) {
@@ -143,12 +103,15 @@ function SignUp() {
 										},
 									},
 								})}
+								placeholder="비밀번호를 한번더 입력해 주세요."
+								required
+								autoComplete="new-password"
 							/>
 						</div>
 						{errors.passwordCheck && (
 							<span>{errors.passwordCheck.message}</span>
 						)}
-						<button type="submit" onClick={SignUp.handleSignup}>
+						<button type="submit">
 							회원가입
 						</button>
 					</form>
