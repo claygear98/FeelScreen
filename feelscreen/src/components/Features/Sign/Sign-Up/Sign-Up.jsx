b5c4ec69bb99e9cdf5233dbbd7c01c2b85bba35d
서원준 <hnk1203@naver.com>
2024-01-18 23:51:05 +0900

    포트 수정

--- feelscreen/src/components/Features/Sign/Sign-Up/Sign-Up.jsx
+++ feelscreen/src/components/Features/Sign/Sign-Up/Sign-Up.jsx
@@ -110,7 +110,7 @@ function SignUp() {
 
 	function lastSubmit(data) {
 		console.log(data);
-		axios.post('http://localhost:3001/sign-up/allow', {
+		axios.post('http://localhost:3000/sign-up/allow', {
 			username: data.username,
 			phone: data.phone,
 			code: data.code,
@@ -118,12 +118,12 @@ function SignUp() {
 		});
 	}
 	function phoneSubmit(data) {
-		axios.get('http://localhost:3001/sign-up/phone', {
+		axios.get('http://localhost:3000/sign-up/phone', {
 			phone: data.phone,
 		});
 	}
 	function codeSubmit(data) {
-		axios.get('http://localhost:3001/sign-up/code', {
+		axios.get('http://localhost:3000/sign-up/code', {
 			code: data.code,
 		});
 	}
