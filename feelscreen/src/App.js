06ec82de1ad855b69d2e8ea04c3a5c4937dae7b6
hnk1203 <hnk1203@naver.com>
2024-01-21 04:04:05 +0900

    에다 tailwind 삭제

--- feelscreen/src/App.js
+++ feelscreen/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import LogIn from './pages/LogIn';
 
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter>
+			<Router>
 				<Routes>
 					<Route path="/sign-up" element={<SignUp />} />
 					<Route path="/log-in" element={<LogIn />} />
 				</Routes>
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 }
