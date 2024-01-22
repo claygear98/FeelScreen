3dcbc58c37169986042bfd56bea23101a6984392
hnk1203 <hnk1203@naver.com>
2024-01-22 16:05:01 +0900

    JWT 임의설정

--- feelscreen/src/components/Features/Sign/Log-In/Log-In.jsx
+++ feelscreen/src/components/Features/Sign/Log-In/Log-In.jsx
@@ -95,8 +95,10 @@ function LogIn() {
 				console.log(response)
 				if (response.status === 201) {
 					navigateToMain();
-					cookies.set("Authorization", response.status);
-					cookies.set("Refresh", response.statusText);
+					const Access = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IuyEnOybkOykgCIsInBob25lIjoiMDEwODgzMTM5NTYiLCJwYXNzd29yZCI6IjEyMzQxMjM0ciJ9.deryl71FsMUfaoiq4pNIxmba9343vIyjpZwrmqA9Gc4";
+					const Refresh = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IuyEnOybkOykgCIsInBob25lIjoiMDEwODgzMTM5NTYiLCJwYXNzd29yZCI6IjEyMzQxMjM0ciJ9.deryl71FsMUfaoiq4pNIxmba9343vIyjpZwrmqA9Gc4";
+					cookies.set("Authorization", Access);
+					cookies.set("Refresh", Refresh);
 				} else {
 					alert('로그인에 실패했습니다. 전화번호/비밀번호를 확인해주세요');
 				}
