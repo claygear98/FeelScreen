55ee2afd55162552a42a173e32820f1eb9737637
hnk1203 <hnk1203@naver.com>
2024-01-23 05:08:12 +0900

    함수틀 만들기 닉네임 수정 구현

--- feelscreen/src/components/Features/MyPage/UserMyPage.jsx
+++ feelscreen/src/components/Features/MyPage/UserMyPage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+// import { Cookies } from 'react-cookie';
+import { useState } from 'react';
 
 const MypageContainer = styled.div`
 	margin-top: 20px;
@@ -28,27 +32,144 @@ const Section = styled.div`
 	}
 `;
 
-const UserWriteList = styled.ul`
-	list-style: none;
-	text-align: left;
-	width: 80%;
+const ProfileModify = styled.form`
+	width: 70%;
+	margin-top: 10px;
+	margin-bottom: 20px;
+	border: 2px solid black;
+	border-radius: 10px;
+	padding: 20px 30px 10px 30px;
+	text-align: right;
+	box-shadow: -5px 5px 10px rgba(0, 0, 0, 0.5);
+	button {
+		margin-top: 20px;
+		font-size: 14.5px;
+		width: 130px;
+		height: 35px;
+		border-radius: 10px;
+		border: 1px solid black;
+		background-color: #d9d9d9;
+	}
 `;
+
+const InputInfo = styled.div`
+	width: 100%;
+	text-align: left;
+	> label {
+		font-size: 15px;
+		font-weight: 700;
+	}
+	> input {
+		margin: 10px 0 0 0;
+		width: 100%;
+		font-size: 12px;
+		border: none;
+		border-bottom: 2px solid black;
+		outline: none;
+		box-shadow: none;
+		transition: 0.3s all;
+	}
+	> input:hover {
+		border-bottom: 2px solid blue;
+	}
+`;
+
+const Error = styled.span`
+	font-size: 11px;
+	font-weight: 600;
+	color: red;
+`;
+
 const MyPage = () => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+		getValues,
+		reset,
+	} = useForm({
+		mode: 'onChange',
+		defaultValues: {
+			username: '',
+		},
+	});
+	const [isProfileModifyVisible, setProfileModifyVisible] = useState(false);
+
+	function onSubmit(data) {
+		console.log(data);
+		axios
+			.post('http://localhost:3001/allow', {
+				username: data.username,
+			})
+			.then((Response) => {
+				console.log(Response);
+				if (Response.status === 201) {
+					alert('아이디(닉네임)이/가 변경되었습니다.');
+					setProfileModifyVisible(false);
+					reset();
+				} else {
+					alert('아이디(닉네임)변경에 실패했습니다 다시시도해주세요.');
+				}
+			});
+	}
+
+	function logOut() {
+		// 홈화면으로 네비게이트
+		// 토큰 삭제
+		alert('로그아웃 되었습니다!');
+	}
+
+	function deleteUser() {
+		//axios.delete 로 토큰 보내기
+	}
+
+	function myPost() {
+		//내가 쓴글로 네비게이트
+	}
+
+	function modifyUsername() {
+		setProfileModifyVisible(true);
+	}
 	return (
 		<MypageContainer>
 			<Section>
-				<button>닉네임 수정</button>
-				<button>로그아웃</button>
+				<button onClick={modifyUsername}>닉네임 수정</button>
+				<button onClick={logOut}>로그아웃</button>
 			</Section>
 			<Section>
-				<button>내가 쓴 글</button>
-				<button className="deleteUser">회원 탈퇴</button>
+				<button onClick={myPost}>내가 쓴 글</button>
+				<button className="deleteUser" onClick={deleteUser}>
+					회원 탈퇴
+				</button>
 			</Section>
-			<UserWriteList>
-				<li>1번글</li>
-				<li>2번글</li>
-				<li>3번글</li>
-			</UserWriteList>
+			{isProfileModifyVisible && (
+				<ProfileModify onSubmit={handleSubmit(onSubmit)}>
+					<InputInfo>
+						<label>아이디(닉네임)</label>
+						<br />
+						<input
+							type="text"
+							{...register('username', {
+								validate: {
+									check: () => {
+										if (
+											getValues('username').length > 10 ||
+											getValues('username').length < 2
+										) {
+											return '아이디는 2~10자이내여야합니다.';
+										}
+									},
+								},
+							})}
+							placeholder="아이디는 2~10자이내여야 합니다."
+							required
+							maxLength={10}
+						/>
+						{errors.username && <Error>{errors.username.message}</Error>}
+					</InputInfo>
+					<button type="submit">변경하기</button>
+				</ProfileModify>
+			)}
 		</MypageContainer>
 	);
 };
--- feelscreen/src/components/Features/Sign/Sign-Up/Sign-Up.jsx
+++ feelscreen/src/components/Features/Sign/Sign-Up/Sign-Up.jsx
@@ -90,7 +90,6 @@ const Submit = styled.div`
 	}
 `;
 
-
 const Button = styled.button`
 	width: 100px;
 	border: 2px solid black;
@@ -130,15 +129,16 @@ function SignUp() {
 				phone: data.phone,
 				code: data.code,
 				password: data.password,
-			}).then(Response => {
-				console.log(Response)
+			})
+			.then((Response) => {
+				console.log(Response);
 				if (Response.status === 201) {
 					alert('회원가입이 완료되었습니다! 로그인화면으로 이동합니다.');
 					navigateToLogIn();
 				} else {
 					alert('회원가입이 실패했습니다! 다시시도해주세요.');
 				}
-			})
+			});
 	}
 
 	const [res, setRes] = useState(0);
@@ -162,194 +162,193 @@ function SignUp() {
 	};
 
 	return (
-	
-			<Container>
-				<FeelLogo>Feel Screen</FeelLogo>
-				<SignUpForm>
-					<h2>
-						회원 가입을 위해 <br />
-						정보를 입력해주세요.
-					</h2>
-					<form onSubmit={handleSubmit(lastSubmit)}>
-						<InputInfo>
-							<label>아이디(닉네임)</label>
-							<br />
+		<Container>
+			<FeelLogo>Feel Screen</FeelLogo>
+			<SignUpForm>
+				<h2>
+					회원 가입을 위해 <br />
+					정보를 입력해주세요.
+				</h2>
+				<form onSubmit={handleSubmit(lastSubmit)}>
+					<InputInfo>
+						<label>아이디(닉네임)</label>
+						<br />
+						<input
+							type="text"
+							{...register('username', {
+								validate: {
+									check: () => {
+										if (
+											getValues('username').length > 10 ||
+											getValues('username').length < 2
+										) {
+											return '아이디는 2~10자이내여야합니다.';
+										}
+									},
+								},
+							})}
+							placeholder="아이디는 2~10자이내여야 합니다."
+							required
+							maxLength={10}
+						/>
+						{errors.username && <Error>{errors.username.message}</Error>}
+					</InputInfo>
+					<InputInfo className="subcheck">
+						<label>전화번호</label>
+						<br />
+						<div>
 							<input
 								type="text"
-								{...register('username', {
-									validate: {
-										check: () => {
-											if (
-												getValues('username').length > 10 ||
-												getValues('username').length < 2
-											) {
-												return '아이디는 2~10자이내여야합니다.';
-											}
-										},
-									},
-								})}
-								placeholder="아이디는 2~10자이내여야 합니다."
-								required
-								maxLength={10}
-							/>
-							{errors.username && <Error>{errors.username.message}</Error>}
-						</InputInfo>
-						<InputInfo className="subcheck">
-							<label>전화번호</label>
-							<br />
-							<div>
-								<input
-									type="text"
-									{...register('phone', {
-										validate: {
-											check: (val) => {
-												if (val.match(/\D+/)) {
-													return "전화번호는 '-' 제외한 숫자만 입력해주세요.";
-												}
-												if (val.length < 11) {
-													return '전화번호11자리를다입력해주세요.';
-												}
-												if (
-													!(val.slice(0,3) === "010")
-												) {
-													return '전화번호는010으로시작해야합니다.';
-												}
-											},
-										},
-									})}
-									minLength={11}
-									maxLength={11}
-									placeholder="전화번호를 입력해 주세요."
-									required
-									className="inputsub"
-								/>
-								<Button
-									type="button"
-									onClick={phoneSubmit}
-									className="phcode-btn"
-									disabled={getValues('phone').length < 11 || !(getValues('phone').slice(0,3) === "010")}
-								>
-									전송하기
-								</Button>
-							</div>
-							{errors.phone && <Error>{errors.phone.message}</Error>}
-						</InputInfo>
-						<InputInfo className="subcheck">
-							<label>인증번호 입력</label>
-							<br />
-							<div>
-								<input
-									type="text"
-									{...register('code', {
-										validate: {
-											check: (val) => {
-												if (val.match(/\D+/)) {
-													return '인증번호는 숫자만 입력하십시오';
-												}
-												if (val.length < 5) {
-													return '인증번호 5자리를 다 입력하십시오';
-												}
-											},
-										},
-									})}
-									minLength={5}
-									maxLength={5}
-									placeholder="인증번호를 입력해 주세요."
-									required
-									className="inputsub"
-								/>
-								<Button
-									type="button"
-									onClick={codeSubmit}
-									className="phcode-btn"
-									disabled={
-										getValues('code').length < 5 ||
-										res !== 201 ||
-										codeOff === false
-									}
-								>
-									{codeOff ? '인증하기' : '인증완료'}
-								</Button>
-							</div>
-							{errors.code && <Error>{errors.code.message}</Error>}
-						</InputInfo>
-						<InputInfo>
-							<label>비밀번호</label>
-							<br />
-							<input
-								type="password"
-								{...register('password', {
+								{...register('phone', {
 									validate: {
 										check: (val) => {
-											if (val.match(/[\W_]/)) {
-												return '특수문자는 입력이 안됩니다요';
-											} else if (
-												!val.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,20}$/)
-											) {
-												return '7~20자의 영문자와 숫자의 조합이어야 합니다.';
+											if (val.match(/\D+/)) {
+												return "전화번호는 '-' 제외한 숫자만 입력해주세요.";
+											}
+											if (val.length < 11) {
+												return '전화번호11자리를다입력해주세요.';
+											}
+											if (!(val.slice(0, 3) === '010')) {
+												return '전화번호는010으로시작해야합니다.';
 											}
 										},
 									},
 								})}
-								placeholder="비밀번호를 입력해 주세요."
+								minLength={11}
+								maxLength={11}
+								placeholder="전화번호를 입력해 주세요."
 								required
-								autoComplete="new-password"
-								maxLength={20}
+								className="inputsub"
 							/>
-							{errors.password && <Error>{errors.password.message}</Error>}
-						</InputInfo>
-						<InputInfo>
-							<label>비밀번호 확인</label>
-							<br />
+							<Button
+								type="button"
+								onClick={phoneSubmit}
+								className="phcode-btn"
+								disabled={
+									getValues('phone').length < 11 ||
+									!(getValues('phone').slice(0, 3) === '010')
+								}
+							>
+								전송하기
+							</Button>
+						</div>
+						{errors.phone && <Error>{errors.phone.message}</Error>}
+					</InputInfo>
+					<InputInfo className="subcheck">
+						<label>인증번호 입력</label>
+						<br />
+						<div>
 							<input
-								type="password"
-								{...register('passwordCheck', {
+								type="text"
+								{...register('code', {
 									validate: {
 										check: (val) => {
-											if (getValues('password') !== val) {
-												return '비밀번호가 일치하지 않습니다.';
+											if (val.match(/\D+/)) {
+												return '인증번호는 숫자만 입력하십시오';
+											}
+											if (val.length < 5) {
+												return '인증번호 5자리를 다 입력하십시오';
 											}
 										},
 									},
 								})}
-								placeholder="비밀번호를 한번더 입력해 주세요."
+								minLength={5}
+								maxLength={5}
+								placeholder="인증번호를 입력해 주세요."
 								required
-								autoComplete="new-password"
-								maxLength={20}
+								className="inputsub"
 							/>
-						</InputInfo>
-						{errors.passwordCheck && (
-							<Error>{errors.passwordCheck.message}</Error>
-						)}
-						<InputInfo>
-							<div>
-								<input
-									type="checkbox"
-									{...register('checkbox', {
-										validate: {
-											check: (val) => {
-												if (val === false) {
-													return '체크박스 확인부탁드립니다.';
-												}
-											},
+							<Button
+								type="button"
+								onClick={codeSubmit}
+								className="phcode-btn"
+								disabled={
+									getValues('code').length < 5 ||
+									res !== 201 ||
+									codeOff === false
+								}
+							>
+								{codeOff ? '인증하기' : '인증완료'}
+							</Button>
+						</div>
+						{errors.code && <Error>{errors.code.message}</Error>}
+					</InputInfo>
+					<InputInfo>
+						<label>비밀번호</label>
+						<br />
+						<input
+							type="password"
+							{...register('password', {
+								validate: {
+									check: (val) => {
+										if (val.match(/[\W_]/)) {
+											return '특수문자는 입력이 안됩니다요';
+										} else if (
+											!val.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{7,20}$/)
+										) {
+											return '7~20자의 영문자와 숫자의 조합이어야 합니다.';
+										}
+									},
+								},
+							})}
+							placeholder="비밀번호를 입력해 주세요."
+							required
+							autoComplete="new-password"
+							maxLength={20}
+						/>
+						{errors.password && <Error>{errors.password.message}</Error>}
+					</InputInfo>
+					<InputInfo>
+						<label>비밀번호 확인</label>
+						<br />
+						<input
+							type="password"
+							{...register('passwordCheck', {
+								validate: {
+									check: (val) => {
+										if (getValues('password') !== val) {
+											return '비밀번호가 일치하지 않습니다.';
+										}
+									},
+								},
+							})}
+							placeholder="비밀번호를 한번더 입력해 주세요."
+							required
+							autoComplete="new-password"
+							maxLength={20}
+						/>
+					</InputInfo>
+					{errors.passwordCheck && (
+						<Error>{errors.passwordCheck.message}</Error>
+					)}
+					<InputInfo>
+						<div>
+							<input
+								type="checkbox"
+								{...register('checkbox', {
+									validate: {
+										check: (val) => {
+											if (val === false) {
+												return '체크박스 확인부탁드립니다.';
+											}
 										},
-									})}
-								/>
-								<p>
-									이용약관 개인정보 수집 및 이용, 마케팅 활용 선택에 모두
-									동의합니다. <Link to="http://www.naver.com">자세히 보기</Link>
-								</p>
-							</div>
-							{errors.checkbox && <Error>{errors.checkbox.message}</Error>}
-						</InputInfo>
-						<hr></hr>
-						<Submit>
-							<Button type="submit">회원가입</Button>
-						</Submit>
-					</form>
-				</SignUpForm>
-			</Container>
-
+									},
+								})}
+							/>
+							<p>
+								이용약관 개인정보 수집 및 이용, 마케팅 활용 선택에 모두
+								동의합니다. <Link to="http://www.naver.com">자세히 보기</Link>
+							</p>
+						</div>
+						{errors.checkbox && <Error>{errors.checkbox.message}</Error>}
+					</InputInfo>
+					<hr></hr>
+					<Submit>
+						<Button type="submit">회원가입</Button>
+					</Submit>
+				</form>
+			</SignUpForm>
+		</Container>
 	);
 }
 
