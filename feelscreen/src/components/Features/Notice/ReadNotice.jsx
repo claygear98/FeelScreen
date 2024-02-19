ecb2ca4941de9f405822fac787a589b33f247973
unknown <palkj2580477@naver.com>
2024-02-19 16:51:22 +0900

    readTest

--- feelscreen/src/components/Features/Notice/ReadNotice.jsx
+++ feelscreen/src/components/Features/Notice/ReadNotice.jsx
@@ -1,8 +1,29 @@
 //not yet
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+const sever_port = 'http://localhost:3001';
 
 const ReadNotice = () => {
-	return <div></div>;
+	const noticeList = useCallback(() => {
+		axios
+			.get(`${sever_port}/notice`)
+			.then((Response) => {
+				if (Response.data.success === true) {
+					alert('조회성공!!');
+				}
+				return Response.data.notice;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+	return (
+		<div>
+			<div></div>
+		</div>
+	);
 };
 
 export default ReadNotice;
