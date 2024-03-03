93b7c133cf3252f6ad28042ed6c0e8566ce03c6a
hnk1203 <hnk1203@naver.com>
2024-03-04 05:51:26 +0900

    test

--- feelscreen/src/components/Features/FeelStagram/FeelStaList.jsx
+++ feelscreen/src/components/Features/FeelStagram/FeelStaList.jsx
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { FaHeart } from 'react-icons/fa';
-import { FaRegHeart } from 'react-icons/fa';
-import { FaRegCommentAlt } from 'react-icons/fa';
-import { Cookies } from 'react-cookie';
-import useHeaderInfo from '../Header/HeadStore';
+import FeelstaItem from './FeelstaItem'; // FeelstaItem 컴포넌트 import
 
 const ListContainer = styled.div`
 	width: 100%;
 	margin: 0 auto;
 	position: relative;
 `;
+
 const ListInfo = styled.div`
 	display: flex;
 	justify-content: space-around;
@@ -26,76 +23,6 @@ const ListItem = styled.ul`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-`;
-
-const Item = styled.li`
-	margin-top: 10px;
-`;
-
-const ItemPreview = styled.div`
-	width: 360px;
-	border-radius: 10px;
-	box-shadow: 1px 1px 7px 0;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-`;
-
-const ItemTop = styled.div`
-	margin-top: 15px;
-	width: 300px;
-	display: flex;
-`;
-
-const NameDate = styled.div`
-	width: 200px;
-	margin-left: 20px;
-	text-align: left;
-	div {
-		margin-top: 10px;
-		color: #555;
-		font-size: 10px;
-	}
-	> div:first-child {
-		margin-top: 15px;
-		color: #222;
-		font-size: 16px;
-	}
-`;
-const ItemSec = styled.div`
-	width: 300px;
-	margin-top: 20px;
-	text-align: left;
-	font-size: 15px;
-	margin-bottom: 10px;
-	div {
-		margin-bottom: 8px;
-	}
-	span {
-		color: #4ecb71;
-		margin-right: 5px;
-	}
-`;
-const ItemImg = styled.div`
-	width: 300px;
-`;
-const ItemBot = styled.div`
-	margin-top: 7px;
-	width: 300px;
-	margin-bottom: 10px;
-	display: flex;
-`;
-
-const Likes = styled.span`
-	span {
-		margin: 0 5px 0 0;
-	}
-`;
-const Comments = styled.span`
-	span {
-		margin: 0 5px 0 0;
-	}
-	margin-left: 10px;
 `;
 
 const Poster = styled.button`
@@ -114,15 +41,10 @@ const Poster = styled.button`
 const FeelStaList = () => {
 	const [feelstaList, setFeelstaList] = useState([]);
 	const [sortList, setSortList] = useState('latest');
-	const [isHeart, setIsHeart] = useState(false);
-	const { username } = useHeaderInfo();
-	const cookies = new Cookies();
 
 	const handleFilterChange = (e) => {
 		setSortList(e.target.value);
 	};
-
-	//검색결과 에 따라서 렌더링 시킬꺼 난항을 겪는중
 
 	const getList = () => {
 		axios.get(`http://localhost:3001/feelsta`).then((response) => {
@@ -130,9 +52,9 @@ const FeelStaList = () => {
 			if (response.data.success === true) {
 				let dataList = response.data.feelsta;
 				if (sortList === 'latest') {
-					dataList = dataList.sort((a, b) => {
-						return new Date(b.FEELSTA_DATE) - new Date(a.FEELSTA_DATE);
-					});
+					dataList = dataList.sort(
+						(a, b) => new Date(b.FEELSTA_DATE) - new Date(a.FEELSTA_DATE)
+					);
 				} else if (sortList === 'likest') {
 					dataList = dataList.sort((a, b) => b.FEELSTA_LIKE - a.FEELSTA_LIKE);
 				}
@@ -141,11 +63,10 @@ const FeelStaList = () => {
 		});
 	};
 
-	// 데이터를 동기적으로 가져오기 위해 useEffect 내부에서 getList 함수 호출
 	useEffect(() => {
 		console.log(2);
 		getList();
-	}, [sortList]); // sortList 전달하여 sortList값이 변경 될 때만 호출되도록 함
+	}, [sortList]);
 
 	const [toSearch, setToSearch] = useState('');
 	const [searchType, setSearchType] = useState('fromtitle');
@@ -168,38 +89,14 @@ const FeelStaList = () => {
 					dataLists = dataLists.filter((item) =>
 						item.FEELSTA_CONTENT.includes(toSearch)
 					);
-					console.log(dataLists);
 				} else if (searchType === 'tagging') {
 					dataLists = dataLists.filter((item) =>
 						item.FEELSTA_TAG.includes(toSearch)
 					);
-					console.log(dataLists);
 				}
 				setFeelstaList(dataLists);
 			}
 		});
-	};
-
-	const handleHeart = (feelstaId) => {
-		if (isHeart === false) {
-			axios
-				.get(`http://localhost:3001/feelstalike`, {
-					headers: {
-						Authorization: cookies.get('Authorization'),
-						feelsta_id: feelstaId,
-					},
-				})
-				.then(setIsHeart(!isHeart));
-		} else {
-			axios
-				.delete(`http://localhost:3001/feelstalike`, {
-					headers: {
-						Authorization: cookies.get('Authorization'),
-						feelsta_id: feelstaId,
-					},
-				})
-				.then(setIsHeart(!isHeart));
-		}
 	};
 
 	const navigate = useNavigate();
@@ -234,68 +131,20 @@ const FeelStaList = () => {
 			</ListInfo>
 			<hr></hr>
 			<ListItem>
-				{feelstaList.map((a, i) => (
-					<Item key={i}>
-						<ItemPreview>
-							<ItemTop>
-								<img
-									src={a.PROFILEIMAGE}
-									alt=""
-									style={{ width: '60px', height: '60px', borderRadius: '50%' }}
-								/>
-								<NameDate>
-									<div>{a.USERNAME}</div>
-									<div>{a.FEELSTA_DATE}</div>
-								</NameDate>
-							</ItemTop>
-							<ItemSec>
-								<div>{a.FEELSTA_CONTENT}</div>
-								{a.FEELSTA_TAG.split(',').map((tag, index) => (
-									<span key={index}>{tag}</span>
-								))}
-							</ItemSec>
-							<ItemImg
-								onClick={() => {
-									navigate(`/feelstadetail/${a.FEELSTA_ID}`, {
-										state: a.FEELSTA_ID,
-									});
-								}}
-							>
-								<img
-									src={a.PROFILEIMAGE}
-									alt=""
-									style={{ width: '300px', borderRadius: '10px' }}
-								/>
-							</ItemImg>
-							<ItemBot>
-								<Likes>
-									{/* {map(a,i)
-										<asdasd {a}/>
-										: setIsHeart(false)} 
-										리스트 따로 빼서 컴포넌트로 만들고 */}
-									<span
-										className="heartPush"
-										onClick={() => {
-											handleHeart(a.FEELSTA_ID);
-										}}
-									>
-										{isHeart ? <FaRegHeart /> : <FaHeart />}
-									</span>
-
-									<span>{a.FEELSTA_LIKE}</span>
-								</Likes>
-								<Comments>
-									<span>
-										<FaRegCommentAlt />
-									</span>
-									<span>{a.COMMENTS}</span>
-								</Comments>
-							</ItemBot>
-						</ItemPreview>
-					</Item>
+				{feelstaList.map((feelsta) => (
+					<FeelstaItem
+						key={feelsta.FEELSTA_ID}
+						PROFILEIMAGE={feelsta.PROFILEIMAGE}
+						USERNAME={feelsta.USERNAME}
+						FEELSTA_DATE={feelsta.FEELSTA_DATE}
+						FEELSTA_CONTENT={feelsta.FEELSTA_CONTENT}
+						FEELSTA_TAG={feelsta.FEELSTA_TAG}
+						FEELSTA_ID={feelsta.FEELSTA_ID}
+						FEELSTA_LIKE={feelsta.FEELSTA_LIKE}
+						COMMENTS={feelsta.COMMENTS}
+					/>
 				))}
 			</ListItem>
-
 			<Poster onClick={() => navigate('/feelstacreate')}>글쓰기</Poster>
 		</ListContainer>
 	);
