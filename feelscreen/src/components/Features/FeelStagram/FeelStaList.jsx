2397dbaff475e1a0d7f3351072b02d9eb775225f
hnk1203 <hnk1203@naver.com>
2024-03-11 05:28:24 +0900

    옵저버 시도1

--- feelscreen/src/components/Features/FeelStagram/FeelStaList.jsx
+++ feelscreen/src/components/Features/FeelStagram/FeelStaList.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import FeelstaItem from './FeelstaItem';
-import { FixedSizeList as List } from 'react-window';
-import AutoSizer from 'react-virtualized-auto-sizer';
-import InfiniteLoader from 'react-window-infinite-loader';
 
 const ListContainer = styled.div`
 	width: 100%;
@@ -16,6 +13,16 @@ const ListContainer = styled.div`
 const ListInfo = styled.div`
 	display: flex;
 	justify-content: space-around;
+`;
+
+const ListItem = styled.ul`
+	width: 100%;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 `;
 
 const Poster = styled.button`
@@ -31,16 +38,40 @@ const Poster = styled.button`
 	opacity: 0.8;
 `;
 
-const SizeContainer = styled.div`
-	width: 400px;
-	height: 500px;
-`;
-
 const FeelStaList = () => {
 	const [feelstaList, setFeelstaList] = useState([]);
+	const [stackList, setStackList] = useState([]);
 	const [sortList, setSortList] = useState('latest');
-	const [page, setPage] = useState(0);
 	const [isLoading, setIsLoading] = useState(false);
+
+	const lastContentRef = useRef(null);
+
+	useEffect(() => {
+		const options = {
+			threshold: 0.5,
+		};
+		const observer = new IntersectionObserver((entries) => {
+			if (entries[0].isIntersecting) {
+				axios.get(`http://localhost:3001/feelsta`).then((response) => {
+					if (response.data.success === true) {
+						let dataLists = response.data.feelsta;
+						setStackList([...feelstaList, dataLists]);
+						setIsLoading(!isLoading);
+					}
+				});
+			} else {
+				setIsLoading(!isLoading);
+			}
+		}, options);
+
+		if (lastContentRef.current) {
+			observer.observe(lastContentRef.current);
+		}
+
+		return () => {
+			observer.disconnect();
+		};
+	}, [isLoading, stackList]);
 
 	const handleFilterChange = (e) => {
 		setSortList(e.target.value);
@@ -64,6 +95,7 @@ const FeelStaList = () => {
 	};
 
 	useEffect(() => {
+		console.log(2);
 		getList();
 	}, [sortList]);
 
@@ -129,41 +161,24 @@ const FeelStaList = () => {
 				</div>
 			</ListInfo>
 			<hr></hr>
-			{/* <ListItem> */}
-			<SizeContainer>
-				<AutoSizer>
-					{({ width, height }) => (
-						<InfiniteLoader>
-							<List
-								height={height}
-								itemCount={feelstaList.length}
-								width={width}
-								itemSize={350}
-							>
-								{({ index, style }) => (
-									<div style={style}>
-										<FeelstaItem
-											key={feelstaList[index].FEELSTA_ID}
-											PROFILEIMAGE={feelstaList[index].PROFILEIMAGE}
-											USERNAME={feelstaList[index].USERNAME}
-											FEELSTA_DATE={feelstaList[index].FEELSTA_DATE}
-											FEELSTA_CONTENT={feelstaList[index].FEELSTA_CONTENT}
-											FEELSTA_TAG={feelstaList[index].FEELSTA_TAG}
-											FEELSTA_ID={feelstaList[index].FEELSTA_ID}
-											FEELSTA_LIKE={feelstaList[index].FEELSTA_LIKE}
-											FEELSTA_IMAGE={feelstaList[index].FEELSTA_IMAGE}
-											COMMENTS={feelstaList[index].COMMENTS}
-											LIKE_NAME={feelstaList[index].LIKE_NAME}
-										/>
-									</div>
-								)}
-							</List>
-						</InfiniteLoader>
-					)}
-				</AutoSizer>
-			</SizeContainer>
-
-			{/* </ListItem> */}
+			<ListItem>
+				{stackList &&
+					stackList.map((feelsta) => (
+						<FeelstaItem
+							key={feelsta.FEELSTA_ID}
+							PROFILEIMAGE={feelsta.PROFILEIMAGE}
+							USERNAME={feelsta.USERNAME}
+							FEELSTA_DATE={feelsta.FEELSTA_DATE}
+							FEELSTA_CONTENT={feelsta.FEELSTA_CONTENT}
+							FEELSTA_TAG={feelsta.FEELSTA_TAG}
+							FEELSTA_ID={feelsta.FEELSTA_ID}
+							FEELSTA_LIKE={feelsta.FEELSTA_LIKE}
+							FEELSTA_IMAGE={feelsta.FEELSTA_IMAGE}
+							COMMENTS={feelsta.COMMENTS}
+							LIKE_NAME={feelsta.LIKE_NAME}
+						/>
+					))}
+			</ListItem>
 			<Poster onClick={() => navigate('/feelstacreate')}>글쓰기</Poster>
 		</ListContainer>
 	);
